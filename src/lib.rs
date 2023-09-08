24f001fca82118267d2d6e8f21c573a868a2f2ad
labreezy <yourtooslow@gmail.com>
2023-09-08 12:30:09 -0500

    funny gcn crate beta version

--- src/lib.rs
+++ src/lib.rs
@@ -1,90 +1,145 @@
 #![no_std]
-#![feature(const_async_blocks)]
-#![feature(type_alias_impl_trait)]
+#![feature(type_alias_impl_trait, const_async_blocks)]
+#![warn(
+    clippy::complexity,
+    clippy::correctness,
+    clippy::perf,
+    clippy::style,
+    clippy::undocumented_unsafe_blocks,
+    rust_2018_idioms
+)]
 
+use asr::{
+    emulator::gcn::{self, Emulator},
+    future::{next_tick, retry},
+    watcher::Watcher, timer::{self, TimerState}, time::Duration,
+};
+use asr::time_util;
 
-
-asr::async_main!(nightly);
 asr::panic_handler!();
-
-
-
-use asr::{Address,
-    future::{next_tick, sleep}, 
-    user_settings::Settings,
-    signature::Signature, Process};
-use bytemuck::CheckedBitPattern;
-
-use core::{time::Duration};
-use core::cmp::min;
-mod state;
-
-#[derive(Settings)]
-struct Settings {
-    //Auto-start upon starting story
-    #[default = true]
-    start_upon_story_start: bool,
-   
-}
-
-const GC_SIG : Signature<10> = Signature::new(
-    "47 53 4E 45 38 50 00 00 01 00"
-);
-
-macro_rules! unwrap_or_continue {
-    ( $e:expr ) => {
-        match $e {
-            Some(x) => x,
-            None => continue,
-        }
-    };
-}
+asr::async_main!(nightly);
 
 async fn main() {
-    // TODO: Set up some general state and settings.
-    let _settings = Settings::register();
-
-    asr::print_message("Hi there!");
+    let settings = Settings::register();
 
     loop {
-        let process = Process::wait_attach("Dolphin.exe").await;
-        process
-            .until_closes(async {
-                let mut dolphin_base: Option<Address> = None;
-                while dolphin_base.is_none() {
-                    asr::print_message("Didn't find it :(");
-                    sleep(Duration::from_secs(2)).await;
-                    dolphin_base = get_gc_base(&process);
-                }
-                loop {
-                    // TODO: Do something on every tick.
-                        
-                    next_tick().await;
-                    break;
-                }
-            })
-            .await;
-    }
-}
-
-fn get_gc_base(proc: &Process) -> Option<Address> {
-    for range in proc.memory_ranges() {
-        if let (Ok(address), Ok(size)) = (range.address(), range.size()) {
-            asr::print_limited::<32>(&format_args!("0x{:x}", address.value())); 
-            let base_addr = GC_SIG.scan_process_range(proc, (address, size)); //guaranteed to be within the first few bytes of a region
-            if base_addr.is_some() {
-                return base_addr;
+        // Hook to the target process
+        let mut emulator = retry(|| gcn::Emulator::attach()).await;
+        let mut watchers = Watchers::default();
+        let offsets = Offsets::new();
+        
+        loop {
+            if !emulator.is_open() {
+                break;
             }
+
+            if emulator.update() {
+                // Splitting logic. Adapted from OG LiveSplit:
+                // Order of execution
+                // 1. update() will always be run first. There are no conditions on the execution of this action.
+                // 2. If the timer is currently either running or paused, then the isLoading, gameTime, and reset actions will be run.
+                // 3. If reset does not return true, then the split action will be run.
+                // 4. If the timer is currently not running (and not paused), then the start action will be run.
+                update_loop(&emulator, &offsets, &mut watchers);
+
+                let timer_state = timer::state();
+                if timer_state == TimerState::Running || timer_state == TimerState::Paused {
+                    if let Some(is_loading) = is_loading(&watchers, &settings) {
+                        if is_loading {
+                            timer::pause_game_time()
+                        } else {
+                            timer::resume_game_time()
+                        }
+                    }
+
+                    if let Some(game_time) = game_time(&watchers, &settings) {
+                        timer::set_game_time(game_time)
+                    }
+
+                    if reset(&watchers, &settings) {
+                        timer::reset()
+                    } else if split(&watchers, &settings) {
+                        timer::split()
+                    }
+                }
+
+                if timer::state() == TimerState::NotRunning && start(&watchers, &settings) {
+                    timer::start();
+                    timer::pause_game_time();
+
+                    if let Some(is_loading) = is_loading(&watchers, &settings) {
+                        if is_loading {
+                            timer::pause_game_time()
+                        } else {
+                            timer::resume_game_time()
+                        }
+                    }
+                }
+            }
+            next_tick().await;
         }
     }
-
-    None
 }
 
-fn read_dolphin<T: CheckedBitPattern+Default>(proc: &Process, base: Address, mut offset: u32) -> T{
-    if offset > 0x4000000 {
-        offset = offset & 0xFFFFFF;
+#[derive(asr::user_settings::Settings)]
+struct Settings {
+    #[default = true]
+    /// START --> Enable auto start
+    start: bool,
+}
+
+#[derive(Default)]
+struct Watchers {
+    igt: Watcher<f32>,
+}
+
+struct Offsets {
+    score: u32,
+}
+
+impl Offsets {
+    fn new() -> Self {
+        Self {
+            score: 0x93DC,
+        }
     }
-    let target_address : Address = base.add(offset as u64);
-    proc.read::<T>(target_address).ok().unwrap_or_default()
+}
+
+
+fn update_loop(game: &Emulator, offsets: &Offsets, watchers: &mut Watchers) {
+    let score = game.read::<u32>(offsets.score).unwrap_or_default();
+
+    watchers.igt.update_infallible(1.0);
+}
+
+fn start(watchers: &Watchers, settings: &Settings) -> bool {
+    if !settings.start {
+        return false;
+    }
+    false
+}
+
+fn split(watchers: &Watchers, settings: &Settings) -> bool {
+    false
+}
+
+fn reset(watchers: &Watchers, settings: &Settings) -> bool {
+    false
+}
+
+fn is_loading(watchers: &Watchers, settings: &Settings) -> Option<bool> {
+    Some(true)
+}
+
+fn game_time(watchers: &Watchers, settings: &Settings) -> Option<Duration> {
+    let current_frames = 3600;
+
+    let seconds = 3.0;
+
+    let igt = Duration::seconds_f32(seconds);
+
+
+    let igt = time_util::frame_count::<60>(current_frames);
+
+    Some(igt)
 }
